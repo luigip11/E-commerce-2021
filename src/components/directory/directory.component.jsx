7057a527f39c785b7c427d5a1965000be3cae252
Luigi Puzziferri <luigi.puzziferri@macnil.local>
2021-06-16 17:20:20 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,64 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-constructor () {
-    super();
-
-    this.state = {
-        sections: [
-            {
-              title: 'CAPPELLI',
-              imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-              id: 1,
-              linkUrl: 'shop/hats'
-            },
-            {
-              title: 'GIACCHE',
-              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-              id: 2,
-              linkUrl: 'shop/jackets'
-            },
-            {
-              title: 'SNEAKERS',
-              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-              id: 3,
-              linkUrl: 'shop/sneakers'
-            },
-            {
-              title: 'DONNA',
-              imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-              size: 'large',
-              id: 4,
-              linkUrl: 'shop/womens'
-            },
-            {
-              title: 'UOMO',
-              imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-              size: 'large',
-              id: 5,
-              linkUrl: 'shop/mens'
-            }
-          ]
-          
-    }
-}
-
-render() {
-    return (
+const Directory = ({ sections }) => (
         <div className='directory-menu'>
-            {
-                this.state.sections.map(({id, ...otherSectionProps}) => (
-                    <MenuItem key={id} {...otherSectionProps}/>
-                ))  //otherSectionProps racchiude title, imageUrl, size, linkUrl. id è la chiave che ci interessa passare
+            {sections.map(({id, ...otherSectionProps}) => (
+              <MenuItem key={id} {...otherSectionProps}/>
+            ))  //otherSectionProps racchiude title, imageUrl, size, linkUrl. id è la chiave che ci interessa passare
             }
         </div>
-    )
-}
+);
 
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect (mapStateToProps)(Directory);
