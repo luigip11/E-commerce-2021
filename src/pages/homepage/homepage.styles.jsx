a80c22fe0aff8e0476ac860a7efa61645ee8b211
Luigi Puzziferri <luigi.puzziferri@macnil.local>
2021-06-17 17:46:03 +0200

    css in js part 1

---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+//CSS in JS andando a creare un oggetto che racchiude i valori da settare
+
+export const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 80px;
+`;
